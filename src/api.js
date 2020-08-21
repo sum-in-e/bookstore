3009b6292cd522bfa625500824e537c81c126388
sumpson0-0 <sumpson00@gmail.com>
2020-08-21 22:25:55 +0900

    api params에 output 추가

--- src/api.js
+++ src/api.js
@@ -10,6 +10,7 @@ export const booksApi = {
 			params: {
 				key: '48FA0EAD055D5F272FC1E1F639CA0D5F329FF47F91BABF2FF518B8E746EFB082',
 				query: item,
+				output: 'json',
 			},
 		}),
 	getList: category =>
@@ -17,6 +18,7 @@ export const booksApi = {
 			params: {
 				key: '48FA0EAD055D5F272FC1E1F639CA0D5F329FF47F91BABF2FF518B8E746EFB082',
 				categoryId: '100',
+				output: 'json',
 			},
 		}),
 };

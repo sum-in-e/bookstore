bb70a668c4063063a5efd1feafcc6bfe800bc66b
sumpson0-0 <sumpson00@gmail.com>
2020-08-24 19:51:24 +0900

    Recommned, NewBook Home 화면에서 보이게 연결

--- src/routes/home/HomePresenter.js
+++ src/routes/home/HomePresenter.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Books } from '@styled-icons/icomoon';
+import ArticleRecommend from 'components/Recommend';
+import NewBookComponent from 'components/NewBook';
+import Preview from 'components/Preview';
 
 const Header = styled.header`
 	display: flex;
@@ -9,7 +12,6 @@ const Header = styled.header`
 	padding: 0 8rem;
 	width: 100%;
 	height: 20vh;
-	font-family: 'Nanum Gothic Coding', monospace;
 `;
 
 const Logo = styled(Books)`
@@ -29,8 +31,6 @@ const Title = styled.h1`
 	cursor: pointer;
 `;
 
-const Form = styled.form``;
-
 const Input = styled.input`
 	width: 35vw;
 	height: 6vh;
@@ -40,68 +40,21 @@ const Input = styled.input`
 `;
 
 const Main = styled.main``;
+const ArticleBestseller = styled.div``;
 
 const SectionTop = styled.section`
-	padding: 0 8rem;
+	padding: 0 10rem;
 	width: 100%;
 	height: 75vh;
 	background-color: #f8f3ed;
 `;
-const ArticleBestseller = styled.article``;
 
 const SectionBottom = styled.section`
 	display: flex;
 	justify-content: space-between;
-	padding: 0 8rem;
+	padding: 2rem 10rem;
 	width: 100%;
-	height: 85vh;
-`;
-const ArticleRecommend = styled.article`
-	width: 70%;
-`;
-const SixGrids = styled.div`
-	display: grid;
-	grid-template-rows: repeat(3, 1fr);
-	grid-template-columns: repeat(2, 1fr);
-	height: 100%;
-`;
-
-const ArticleAttention = styled.article`
-	width: 20%;
-`;
-const ThreeGrids = styled.div`
-	display: grid;
-	grid-template-rows: repeat(4, 1fr);
-	grid-template-columns: 1fr;
-	height: 100%;
-`;
-
-const H2 = styled.h2`
-	color: #00317d;
-`;
-
-const Item = styled.div`
-	display: flex;
-	padding: 2rem;
-`;
-const Cover = styled.div`
-	background-image: url('https://images.unsplash.com/photo-1537495329792-41ae41ad3bf0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80');
-	background-size: cover;
-	width: 30%;
-	height: auto;
-`;
-const Info = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	padding-left: 1rem;
-`;
-const H3 = styled.h3`
-	padding-bottom: 1rem;
-	color: #00317d;
-`;
-const P = styled.p`
-	color: #e3900e;
+	height: 90vh;
 `;
 
 const Footer = styled.footer`
@@ -110,99 +63,65 @@ const Footer = styled.footer`
 	height: 25vh;
 	background-color: grey;
 `;
-const HomePresenter = () => {
-	return (
-		<>
-			<Header>
-				<Logo></Logo>
-				<Title>Book Store</Title>
-				<Form>
-					<Input type="text"></Input>
-				</Form>
-			</Header>
-			<Main>
-				<SectionTop>
-					<ArticleBestseller></ArticleBestseller>
-				</SectionTop>
-				<SectionBottom>
-					<ArticleRecommend>
-						<H2>편집장 추천 도서</H2>
-						<SixGrids>
-							<Item>
-								<Cover></Cover>
-								<Info>
-									<H3>여자 둘이 살고 있습니다.</H3>
-									<P>15,350원</P>
-								</Info>
-							</Item>
-							<Item>
-								<Cover></Cover>
-								<Info>
-									<H3>여자 둘이 살고 있습니다.</H3>
-									<P>15,350원</P>
-								</Info>
-							</Item>
-							<Item>
-								<Cover></Cover>
-								<Info>
-									<H3>여자 둘이 살고 있습니다.</H3>
-									<P>15,350원</P>
-								</Info>
-							</Item>
-							<Item>
-								<Cover></Cover>
-								<Info>
-									<H3>여자 둘이 살고 있습니다.</H3>
-									<P>15,350원</P>
-								</Info>
-							</Item>
-							<Item>
-								<Cover></Cover>
-								<Info>
-									<H3>여자 둘이 살고 있습니다.</H3>
-									<P>15,350원</P>
-								</Info>
-							</Item>
-							<Item>
-								<Cover></Cover>
-								<Info>
-									<H3>여자 둘이 살고 있습니다.</H3>
-									<P>15,350원</P>
-								</Info>
-							</Item>
-						</SixGrids>
+
+const HomePresenter = ({ bestseller, recommendation, newBook, loading }) => (
+	<>
+		<Header>
+			<Logo></Logo>
+			<Title>Book Store</Title>
+			<form>
+				<Input type="text"></Input>
+			</form>
+		</Header>
+		<Main>
+			<SectionTop>
+				<ArticleBestseller></ArticleBestseller>
+			</SectionTop>
+			<SectionBottom>
+				{recommendation && recommendation.length > 0 && (
+					<ArticleRecommend title="추천 도서">
+						{recommendation.map(book => (
+							<Preview
+								key={book.itemId}
+								title={book.title}
+								author={book.author}
+								publisher={book.publisher}
+								price={book.priceSales}
+								description={book.description}
+								pubDate={book.pubDate}
+								saleStatus={book.saleStatus}
+								coverImageS={book.coverSmallUrl}
+								coverImageL={book.coverLargeUrl}
+								isbn={book.isbn}
+								buyLink={book.link}
+							/>
+						))}
 					</ArticleRecommend>
-					<ArticleAttention>
-						<H2>주목할만한 신간</H2>
-						<ThreeGrids>
-							<Item>
-								<Cover></Cover>
-								<Info>
-									<H3>여자 둘이 살고 있습니다.</H3>
-									<P>15,350원</P>
-								</Info>
-							</Item>
-							<Item>
-								<Cover></Cover>
-								<Info>
-									<H3>여자 둘이 살고 있습니다.</H3>
-									<P>15,350원</P>
-								</Info>
-							</Item>
-							<Item>
-								<Cover></Cover>
-								<Info>
-									<H3>여자 둘이 살고 있습니다.</H3>
-									<P>15,350원</P>
-								</Info>
-							</Item>
-						</ThreeGrids>
-					</ArticleAttention>
-				</SectionBottom>
-			</Main>
-			<Footer></Footer>
-		</>
-	);
-};
+				)}
+				{newBook && newBook.length > 0 && (
+					<NewBookComponent title="신간 도서">
+						{newBook.map(book => (
+							<Preview
+								key={book.itemId}
+								title={book.title}
+								author={book.author}
+								publisher={book.publisher}
+								price={book.priceSales}
+								description={book.description}
+								pubDate={book.pubDate}
+								saleStatus={book.saleStatus}
+								coverImageS={book.coverSmallUrl}
+								coverImageL={book.coverLargeUrl}
+								isbn={book.isbn}
+								buyLink={book.link}
+							/>
+						))}
+					</NewBookComponent>
+				)}
+			</SectionBottom>
+		</Main>
+		<Footer></Footer>
+	</>
+);
 
 export default HomePresenter;

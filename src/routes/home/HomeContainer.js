21239fc042fbbd153c96619f51d6d090b18cf357
sumpson0-0 <sumpson00@gmail.com>
2020-08-24 19:50:19 +0900

    API에서 데이터 받아와 Home Presenter로 넘겨주는 기능 구현

--- src/routes/home/HomeContainer.js
+++ src/routes/home/HomeContainer.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import HomePresenter from './HomePresenter';
+import { booksApi } from 'api';
 
 class HomeContainer extends React.Component {
+	state = {
+		bestseller: null,
+		recommendation: null,
+		newBook: null,
+		loading: false,
+	};
+
+	async componentDidMount() {
+		const {
+			data: { item: getBestseller },
+		} = await booksApi.getList('bestSeller.api');
+		const {
+			data: { item: getRecommendation },
+		} = await booksApi.getList('recommend.api');
+		const {
+			data: { item: getNewBook },
+		} = await booksApi.getList('newBook.api');
+		this.setState({
+			bestseller: getBestseller.slice(0, 10),
+			recommendation: getRecommendation.slice(0, 6),
+			newBook: getNewBook.slice(0, 3),
+			loading: true,
+		});
+	}
+
 	render() {
-		return <HomePresenter />;
+		const { bestseller, recommendation, newBook, loading } = this.state;
+		console.log(newBook);
+		return (
+			<HomePresenter
+				bestseller={bestseller}
+				recommendation={recommendation}
+				newBook={newBook}
+				loading={loading}
+			/>
+		);
 	}
 }
 

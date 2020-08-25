c3544bbef793803b69007d7fa2418af6f4117397
sumpson0-0 <sumpson00@gmail.com>
2020-08-25 21:11:44 +0900

    Loader Component 연결

--- src/routes/home/HomePresenter.js
+++ src/routes/home/HomePresenter.js
@@ -5,6 +5,7 @@ import ArticleNewBook from 'components/NewBook';
 import Preview from 'components/Preview';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
+import Loader from 'components/Loader';
 
 const Main = styled.main``;
 
@@ -27,55 +28,61 @@ const SectionBottom = styled.section`
 
 const HomePresenter = ({ bestseller, recommendation, newBook, loading }) => (
 	<>
-		<Header />
-		<Main>
-			<SectionTop>
-				<ArticleBestseller />
-			</SectionTop>
-			<SectionBottom>
-				{recommendation && recommendation.length > 0 && (
-					<ArticleRecommend title="추천 도서">
-						{recommendation.map(book => (
-							<Preview
-								key={book.itemId}
-								title={book.title}
-								author={book.author}
-								publisher={book.publisher}
-								price={book.priceSales}
-								description={book.description}
-								pubDate={book.pubDate}
-								saleStatus={book.saleStatus}
-								coverImageS={book.coverSmallUrl}
-								coverImageL={book.coverLargeUrl}
-								isbn={book.isbn}
-								buyLink={book.link}
-							/>
-						))}
-					</ArticleRecommend>
-				)}
-				{newBook && newBook.length > 0 && (
-					<ArticleNewBook title="신간 도서">
-						{newBook.map(book => (
-							<Preview
-								key={book.itemId}
-								title={book.title}
-								author={book.author}
-								publisher={book.publisher}
-								price={book.priceSales}
-								description={book.description}
-								pubDate={book.pubDate}
-								saleStatus={book.saleStatus}
-								coverImageS={book.coverSmallUrl}
-								coverImageL={book.coverLargeUrl}
-								isbn={book.isbn}
-								buyLink={book.link}
-							/>
-						))}
-					</ArticleNewBook>
-				)}
-			</SectionBottom>
-		</Main>
-		<Footer />
+		{loading ? (
+			<Loader />
+		) : (
+			<>
+				<Header />
+				<Main>
+					<SectionTop>
+						<ArticleBestseller />
+					</SectionTop>
+					<SectionBottom>
+						{recommendation && recommendation.length > 0 && (
+							<ArticleRecommend title="추천 도서">
+								{recommendation.map(book => (
+									<Preview
+										key={book.itemId}
+										title={book.title}
+										author={book.author}
+										publisher={book.publisher}
+										price={book.priceSales}
+										description={book.description}
+										pubDate={book.pubDate}
+										saleStatus={book.saleStatus}
+										coverImageS={book.coverSmallUrl}
+										coverImageL={book.coverLargeUrl}
+										isbn={book.isbn}
+										buyLink={book.link}
+									/>
+								))}
+							</ArticleRecommend>
+						)}
+						{newBook && newBook.length > 0 && (
+							<ArticleNewBook title="신간 도서">
+								{newBook.map(book => (
+									<Preview
+										key={book.itemId}
+										title={book.title}
+										author={book.author}
+										publisher={book.publisher}
+										price={book.priceSales}
+										description={book.description}
+										pubDate={book.pubDate}
+										saleStatus={book.saleStatus}
+										coverImageS={book.coverSmallUrl}
+										coverImageL={book.coverLargeUrl}
+										isbn={book.isbn}
+										buyLink={book.link}
+									/>
+								))}
+							</ArticleNewBook>
+						)}
+					</SectionBottom>
+				</Main>
+				<Footer />
+			</>
+		)}
 	</>
 );
 
--- src/routes/itemList/ItemListPresenter.js
+++ src/routes/itemList/ItemListPresenter.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Header from 'components/Header';
 import Item from 'components/Item';
 import Footer from 'components/Footer';
+import Loader from 'components/Loader';
 
 const Main = styled.main`
 	padding: 0 10rem;
@@ -22,30 +23,36 @@ const Section = styled.div`
 
 const ItemListPresenter = ({ items, params, loading }) => (
 	<>
-		<Header />
-		<Main>
-			{params && params.length > 0 && params === 'bestSeller' && <Title>베스트셀러</Title>}
-			{params && params.length > 0 && params === 'recommend' && <Title>추천 도서</Title>}
-			{params && params.length > 0 && params === 'newBook' && <Title>신작 도서</Title>}
-			{items && items.length > 0 && (
-				<Section>
-					{items.map(item => (
-						<Item
-							key={item.itemId}
-							title={item.title}
-							author={item.author}
-							saleStatus={item.saleStatus}
-							publisher={item.publisher}
-							pubDate={item.pubDate}
-							price={item.priceSales}
-							reviewRank={item.customerReviewRank}
-							cover={item.coverSmallUrl}
-						/>
-					))}
-				</Section>
-			)}
-		</Main>
-		<Footer />
+		{loading ? (
+			<Loader />
+		) : (
+			<>
+				<Header />
+				<Main>
+					{params && params.length > 0 && params === 'bestSeller' && <Title>베스트셀러</Title>}
+					{params && params.length > 0 && params === 'recommend' && <Title>추천 도서</Title>}
+					{params && params.length > 0 && params === 'newBook' && <Title>신작 도서</Title>}
+					{items && items.length > 0 && (
+						<Section>
+							{items.map(item => (
+								<Item
+									key={item.itemId}
+									title={item.title}
+									author={item.author}
+									saleStatus={item.saleStatus}
+									publisher={item.publisher}
+									pubDate={item.pubDate}
+									price={item.priceSales}
+									reviewRank={item.customerReviewRank}
+									cover={item.coverSmallUrl}
+								/>
+							))}
+						</Section>
+					)}
+				</Main>
+				<Footer />
+			</>
+		)}
 	</>
 );
 

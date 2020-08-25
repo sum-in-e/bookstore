9d46583a539d2656f5d987091eb56b56653aaa0a
sumpson0-0 <sumpson00@gmail.com>
2020-08-25 14:59:06 +0900

    ItemList API 호출 및 보여지게 구현

--- src/routes/itemList/ItemListContainer.js
+++ src/routes/itemList/ItemListContainer.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ItemListPresenter from './ItemListPresenter';
+import { booksApi } from 'api';
 
 class ItemListContainer extends React.Component {
+	state = {
+		items: null,
+		params: null,
+		loading: true,
+	};
+
+	async componentDidMount() {
+		try {
+			const params = this.props.match.params.queryType;
+			const {
+				data: { item: items },
+			} = await booksApi.getList(params);
+			this.setState({ items, params });
+		} catch (error) {
+			console.log(error);
+		} finally {
+			this.setState({ loading: false });
+		}
+	}
+
 	render() {
-		return <ItemListPresenter />;
+		const { items, params, loading } = this.state;
+		console.log(items);
+		return <ItemListPresenter items={items} params={params} loading={loading} />;
 	}
 }
 
--- src/routes/itemList/ItemListPresenter.js
+++ src/routes/itemList/ItemListPresenter.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from 'components/Header';
+import Item from 'components/Item';
 
-const ItemListPresenter = () => {
-	return <h1>Item List</h1>;
-};
+const Main = styled.main`
+	padding: 0 10rem;
+	background-color: #f8f3ed;
+`;
+
+const Title = styled.h1`
+	padding: 2rem 0 2rem 7.5rem;
+	font-size: 1.6rem;
+`;
+
+const Section = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const ItemListPresenter = ({ items, params, loading }) => (
+	<>
+		<Header></Header>
+		<Main>
+			{params && params.length > 0 && params == 'bestSeller' && <Title>베스트셀러</Title>}
+			{params && params.length > 0 && params == 'recommend' && <Title>추천 도서</Title>}
+			{params && params.length > 0 && params == 'newBook' && <Title>신작 도서</Title>}
+			{items && items.length > 0 && (
+				<Section>
+					{items.map(item => (
+						<Item
+							key={item.itemId}
+							title={item.title}
+							author={item.author}
+							saleStatus={item.saleStatus}
+							publisher={item.publisher}
+							pubDate={item.pubDate}
+							price={item.priceSales}
+							reviewRank={item.customerReviewRank}
+							cover={item.coverSmallUrl}
+						/>
+					))}
+				</Section>
+			)}
+		</Main>
+	</>
+);
 
 export default ItemListPresenter;

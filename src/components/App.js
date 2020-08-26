a16f8773af17b7c7a97fae02e47e18b124f99e35
sumpson0-0 <sumpson00@gmail.com>
2020-08-26 19:31:14 +0900

    GlobalStyles 파일명 수정

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // react에서 제공하는 Class Component를 상속받아 사용하기 위해 import
 // {Component} import 안하면 Component 없다는 에러 뜸
 import Router from './Router';
-import GlobalStyles from './GlobalStyleds';
+import GlobalStyles from './GlobalStyles';
 
 class App extends Component {
 	render() {

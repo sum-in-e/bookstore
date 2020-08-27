3868a5bf56efd635f7c0c4d9f96333e8ce1296c6
sumpson0-0 <sumpson00@gmail.com>
2020-08-27 20:56:14 +0900

    Creact Search Component

--- src/routes/search/SearchContainer.js
+++ src/routes/search/SearchContainer.js
@@ -1,9 +1,71 @@
 import React from 'react';
 import SearchPresenter from './SearchPresenter';
+import { booksApi } from 'api';
 
 class SearchContainer extends React.Component {
+	state = {
+		results: null,
+		params: null,
+		searchTerm: null,
+		loading: true,
+	};
+
+	handleSubmit = event => {
+		event.preventDefault();
+		const { searchTerm } = this.state;
+		if (searchTerm !== '') {
+			this.props.history.push(`/search/${searchTerm}`);
+			this.searchByTerm(searchTerm);
+		}
+	};
+
+	handleChange = event => {
+		const {
+			target: { value: searchTerm },
+		} = event;
+		this.setState({ searchTerm });
+	};
+
+	searchByTerm = async searchTerm => {
+		this.setState({ loading: true });
+		try {
+			const {
+				data: { item: results },
+			} = await booksApi.search(searchTerm);
+			this.setState({ results, params: searchTerm });
+		} catch (error) {
+			console.log(error);
+		} finally {
+			this.setState({ loading: false });
+		}
+	};
+
+	async componentDidMount() {
+		try {
+			const params = this.props.match.params.item;
+			const {
+				data: { item: results },
+			} = await booksApi.search(params);
+			this.setState({ results, params });
+			console.log(results);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			this.setState({ loading: false });
+		}
+	}
+
 	render() {
-		return <SearchPresenter />;
+		const { results, params, loading } = this.state;
+		return (
+			<SearchPresenter
+				results={results}
+				loading={loading}
+				params={params}
+				handleSubmit={this.handleSubmit}
+				handleChange={this.handleChange}
+			/>
+		);
 	}
 }
 
--- src/routes/search/SearchPresenter.js
+++ src/routes/search/SearchPresenter.js
@@ -1,7 +1,70 @@
 import React from 'react';
+import styled from 'styled-components';
+import Loader from 'components/Loader';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+import Item from 'components/Item';
 
-const SearchPresenter = () => {
-	return <h1>Search</h1>;
-};
+const Main = styled.main`
+	padding: 0 10rem;
+	background-color: #f8f3ed;
+`;
+
+const Title = styled.h1`
+	padding: 2rem 0 2rem 7.5rem;
+	font-size: 1.6rem;
+`;
+
+const Section = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const NoResults = styled.h1`
+	height: 50vh;
+	padding: 2rem 0 2rem 7.5rem;
+	font-size: 1.6rem;
+`;
+
+const SearchPresenter = ({ results, params, loading, handleSubmit, handleChange }) => (
+	<>
+		{loading ? (
+			<Loader />
+		) : (
+			<>
+				<Header handleSubmit={handleSubmit} handleChange={handleChange} />
+				<Main>
+					{results && results.length === 0 && (
+						<NoResults>{`'${params}'에 대한 검색 결과가 없습니다.`}</NoResults>
+					)}
+					{results && results.length > 0 && <Title>{`'${params}'에 대한 검색 결과`}</Title>}
+					{results && results.length > 0 && (
+						<Section>
+							{results.map(item => (
+								<Item
+									key={item.itemId}
+									title={item.title}
+									author={item.author}
+									publisher={item.publisher}
+									price={item.priceSales}
+									description={item.description}
+									pubDate={item.pubDate}
+									saleStatus={item.saleStatus}
+									reviewRank={item.customerReviewRank}
+									coverImageS={item.coverSmallUrl}
+									coverImageL={item.coverLargeUrl}
+									isbn={item.isbn}
+									buyLink={item.link}
+								/>
+							))}
+						</Section>
+					)}
+				</Main>
+				<Footer />
+			</>
+		)}
+	</>
+);
 
 export default SearchPresenter;

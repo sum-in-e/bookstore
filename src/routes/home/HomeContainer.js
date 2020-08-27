712076a608c4f622323551ac7415c02445308f26
sumpson0-0 <sumpson00@gmail.com>
2020-08-27 14:58:28 +0900

    캐러셀 setting 수정

--- src/routes/home/HomeContainer.js
+++ src/routes/home/HomeContainer.js
@@ -14,7 +14,9 @@ class HomeContainer extends React.Component {
 			speed: 500,
 			slidesToShow: 1,
 			slidesToScroll: 1,
-			arrows: true,
+			arrows: false,
+			autoplay: true,
+			autoplaySpeed: 3000,
 		},
 	};
 

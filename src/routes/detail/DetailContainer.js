2158a0602cc7c3520587f95c33263cd7110d18a7
sumpson0-0 <sumpson00@gmail.com>
2020-08-27 14:59:14 +0900

    Create Detail Page

--- src/routes/detail/DetailContainer.js
+++ src/routes/detail/DetailContainer.js
@@ -2,8 +2,82 @@ import React from 'react';
 import DetailPresenter from './DetailPresenter';
 
 class DetailContainer extends React.Component {
+	state = {
+		title: null,
+		author: null,
+		saleStatus: null,
+		publisher: null,
+		pubDate: null,
+		price: null,
+		description: null,
+		reviewRank: null,
+		coverImageS: null,
+		coverImageL: null,
+		isbn: null,
+		buyLink: null,
+		loading: true,
+	};
+
+	handleClick = event => {
+		event.preventDefault();
+		window.open(this.state.buyLink);
+	};
+
+	async componentDidMount() {
+		try {
+			const book = await this.props.location.state;
+			this.setState({
+				title: book.title,
+				author: book.author,
+				saleStatus: book.saleStatus,
+				publisher: book.publisher,
+				pubDate: book.pubDate,
+				price: book.price,
+				description: book.description,
+				reviewRank: book.reviewRank,
+				coverImageS: book.coverImageS,
+				coverImageL: book.coverImageL,
+				isbn: book.isbn,
+				buyLink: book.buyLink,
+			});
+			window.scrollTo(0, 0);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			this.setState({ loading: false });
+		}
+	}
+
 	render() {
-		return <DetailPresenter />;
+		const {
+			title,
+			author,
+			saleStatus,
+			publisher,
+			pubDate,
+			price,
+			description,
+			reviewRank,
+			coverImageL,
+			isbn,
+			loading,
+		} = this.state;
+		return (
+			<DetailPresenter
+				title={title}
+				author={author}
+				saleStatus={saleStatus}
+				publisher={publisher}
+				pubDate={pubDate}
+				price={price}
+				description={description}
+				reviewRank={reviewRank}
+				coverImageL={coverImageL}
+				isbn={isbn}
+				loading={loading}
+				handleClick={this.handleClick}
+			/>
+		);
 	}
 }
 
--- src/routes/detail/DetailPresenter.js
+++ src/routes/detail/DetailPresenter.js
@@ -1,7 +1,154 @@
 import React from 'react';
+import styled from 'styled-components';
+import Loader from 'components/Loader';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
 
-const DetailPresenter = () => {
-	return <h1>Detail</h1>;
-};
+const Main = styled.main`
+	width: 100%;
+	height: 80vh;
+	padding: 0 10rem;
+`;
+
+const HeaderSection = styled.section`
+	width: 100%;
+	height: 20%;
+	padding: 1rem 0;
+	border-bottom: 1px solid grey;
+`;
+
+const Title = styled.h1`
+	padding-bottom: 1rem;
+	font-size: 2rem;
+	font-weight: bold;
+`;
+
+const Article = styled.article`
+	display: flex;
+	padding-bottom: 0.4rem;
+	font-size: 1.1rem;
+`;
+
+const Author = styled.p`
+	padding-right: 0.5rem;
+	border-right: 1px solid black;
+`;
+
+const Publisher = styled.p`
+	padding: 0 0.5rem;
+	border-right: 1px solid black;
+`;
+
+const PubDate = styled.p`
+	padding: 0 0.5rem;
+`;
+const InfoSection = styled.section`
+	display: flex;
+	position: relative;
+	width: 90%;
+	height: 80%;
+	margin: 2rem 0;
+`;
+
+const Cover = styled.div`
+	width: 120rem;
+	height: auto;
+	margin-left: 1rem;
+	background-image: url(${props => props.url});
+	background-repeat: no-repeat;
+	background-size: contain;
+`;
+const Info = styled.article`
+	display: flex;
+	flex-direction: column;
+	padding-left: 2rem;
+`;
+const SaleStatus = styled.p`
+	width: 5rem;
+	height: 2rem;
+	margin-bottom: 1.5rem;
+	padding: 0.5rem 0;
+	border: 1px grey solid;
+	border-radius: 3px;
+	text-align: center;
+`;
+
+const Price = styled.p`
+	padding-bottom: 1rem;
+	font-size: 1.3rem;
+	color: #e3900e;
+`;
+
+const Isbn = styled.p`
+	padding-bottom: 2rem;
+`;
+
+const Description = styled.p`
+	line-height: 1.1rem;
+	padding-bottom: 1rem;
+`;
+
+const BuyBtn = styled.button`
+	width: 7.5rem;
+	height: 3rem;
+	background-color: #00317d;
+	color: white;
+	margin-top: 1rem;
+	border: 1px solid grey;
+	outline: none;
+	font-size: 1.1rem;
+	cursor: pointer;
+`;
+
+const DetailPresenter = ({
+	title,
+	author,
+	saleStatus,
+	publisher,
+	pubDate,
+	price,
+	description,
+	reviewRank,
+	coverImageL,
+	isbn,
+	loading,
+	handleClick,
+}) => (
+	<>
+		{loading ? (
+			<Loader />
+		) : (
+			<>
+				<Header></Header>
+				<Main>
+					<HeaderSection>
+						<Title>{title}</Title>
+						<Article>
+							<Author>{author ? author : '저자 미상'}</Author>
+							<Publisher>{publisher}</Publisher>
+							<PubDate>
+								{pubDate
+									? `${pubDate.slice(0, 4)}-${pubDate.slice(4, 6)}-${pubDate.slice(6, 8)}`
+									: '0000-00-00'}
+							</PubDate>
+						</Article>
+					</HeaderSection>
+					<InfoSection>
+						<Cover url={coverImageL ? coverImageL : require('../../assets/image_not_found.jpg')} />
+						<Info>
+							<SaleStatus>{saleStatus}</SaleStatus>
+							<Price>{`판매가       ${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원`}</Price>
+							<Isbn>{`isbn       ${isbn}`}</Isbn>
+							<Description>{description}</Description>
+							<span role="img" aria-label="rank">{`⭐️ ${reviewRank}/10`}</span>
+							<BuyBtn onClick={handleClick}>구매하기</BuyBtn>
+						</Info>
+					</InfoSection>
+				</Main>
+				<Footer></Footer>
+			</>
+		)}
+	</>
+);
 
 export default DetailPresenter;

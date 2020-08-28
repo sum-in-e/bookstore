5d1a5389da7bc9edd41929e477a88f1b6614ff15
sumpson0-0 <sumpson00@gmail.com>
2020-08-28 20:07:15 +0900

    theme.js 연결

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { ThemeProvider } from 'styled-components';
 import Router from './Router';
 import GlobalStyles from 'styles/GlobalStyles';
+import theme from 'styles/theme';
 
 class App extends Component {
 	render() {
 		return (
 			<>
-				<Router />
+				<ThemeProvider theme={theme}>
+					<Router />
+				</ThemeProvider>
 				<GlobalStyles />
 			</>
 		);

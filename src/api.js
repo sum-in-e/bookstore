575bdac329135bd5cf30567242660bd76b3415f8
Kim Sumin <66582899+sumpson0-0@users.noreply.github.com>
2021-03-15 16:05:15 +0900

    serveless functions 사용한 데이터 요청

--- src/api.js
+++ src/api.js
@@ -1,18 +1,13 @@
 import axios from 'axios';
 
-const api = axios.create({
-	baseURL: '/api/',
-});
-
 export const booksApi = {
 	search: item =>
-		api.get('search.api', {
-			params: {
-				key: process.env.REACT_APP_API_KEY,
-				query: item,
-				output: 'json',
-			},
+		axios.post(`api/searchApi`, {
+			item : item
 		}),
+
 	getList: category =>
-		api.get(`sendBooks`),
+		axios.post(`api/getListApi`, {
+			category : category
+		}) 
 };
